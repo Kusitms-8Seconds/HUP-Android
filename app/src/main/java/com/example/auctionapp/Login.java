add16d1225ce359f9f30879db8cc6af36bf081ea
gom1n <alice436777@gmail.com>
2021-11-03 03:48:27 +0900

    [feat] Social logins

---
+++ app/src/main/java/com/example/auctionapp/Login.java
@@ -0,0 +1,329 @@
+package com.example.auctionapp;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.kakao.auth.AuthType;
+import com.kakao.auth.ISessionCallback;
+import com.kakao.auth.Session;
+import com.kakao.network.ErrorResult;
+import com.kakao.usermgmt.UserManagement;
+import com.kakao.usermgmt.callback.LogoutResponseCallback;
+import com.kakao.usermgmt.callback.MeV2ResponseCallback;
+import com.kakao.usermgmt.response.MeV2Response;
+import com.kakao.usermgmt.response.model.Profile;
+import com.kakao.usermgmt.response.model.UserAccount;
+import com.kakao.util.OptionalBoolean;
+import com.kakao.util.exception.KakaoException;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import static android.content.ContentValues.TAG;
+
+import com.nhn.android.naverlogin.OAuthLogin;
+import com.nhn.android.naverlogin.OAuthLoginHandler;
+
+public class Login extends AppCompatActivity {
+
+    Button loginBtn;
+    Button btn_kakao_login;
+    Button btn_google_login;
+    ImageView btn_naver_login;
+    EditText edit_id;
+    EditText edit_pw;
+
+    private SessionCallback sessionCallback = new SessionCallback();
+    Session session;
+    GoogleSignInClient mGoogleSignInClient;
+    private int RC_SIGN_IN = 0116;  //google login request code
+    OAuthLogin mOAuthLoginModule;
+    Context mContext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        //일반 로그인
+        loginBtn = (Button) findViewById(R.id.loginBtn);
+        edit_id = (EditText) findViewById(R.id.edit_id);
+        edit_pw = (EditText) findViewById(R.id.edit_pw);
+        CheckBox autochecked = (CheckBox) findViewById(R.id.autoLogin);
+        SharedPreferences auto = getSharedPreferences("auto", Activity.MODE_PRIVATE);
+        SharedPreferences.Editor editor = auto.edit();
+        //자동 로그인 체크 여부
+        Boolean checked = auto.getBoolean("checked", false);
+        if(checked == true) {
+            onBackPressed();
+        }
+        else {
+            //자동로그인 체크되어있을 때
+            autochecked.setOnClickListener(new CheckBox.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    editor.putBoolean("checked", true);
+                    editor.commit();
+                }
+            }) ;
+            loginBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    String id = edit_id.getText().toString();
+                    String pw = edit_pw.getText().toString();
+                    editor.putString("id", id);
+                    editor.putString("pw", pw);
+                    editor.commit();
+
+                    onBackPressed();
+                }
+            });
+        }
+
+        //카카오 로그인
+        btn_kakao_login = (Button)findViewById(R.id.btn_kakao_login);
+        btn_kakao_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //kakao login
+                session = Session.getCurrentSession();
+                session.addCallback(sessionCallback);
+                session.open(AuthType.KAKAO_LOGIN_ALL, Login.this);
+            }
+        });
+        //구글 로그인
+        btn_google_login = (Button) findViewById(R.id.btn_google_login);
+        btn_google_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // 앱에 필요한 사용자 데이터를 요청하도록 로그인 옵션을 설정한다.
+                // DEFAULT_SIGN_IN parameter는 유저의 ID와 기본적인 프로필 정보를 요청하는데 사용된다.
+                GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                        .requestEmail() // email addresses도 요청함
+                        .build();
+
+                // 위에서 만든 GoogleSignInOptions을 사용해 GoogleSignInClient 객체를 만듬
+                mGoogleSignInClient = GoogleSignIn.getClient(Login.this, gso);
+
+                // 기존에 로그인 했던 계정을 확인한다.
+                GoogleSignInAccount gsa = GoogleSignIn.getLastSignedInAccount(Login.this);
+                // 로그인 되어있는 경우
+                if (gsa != null) {
+                    onBackPressed();
+                } else
+                    googleSignIn();
+            }
+        });
+        //네이버 로그인
+        btn_naver_login = (ImageView)findViewById(R.id.btn_naver_login);
+        mContext = getApplicationContext();
+        btn_naver_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                naverSignIn();
+            }
+        });
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        // 세션 콜백 삭제
+        Session.getCurrentSession().removeCallback(sessionCallback);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        // 카카오톡|스토리 간편로그인 실행 결과를 받아서 SDK로 전달
+        if (Session.getCurrentSession().handleActivityResult(requestCode, resultCode, data)) {
+            return;
+        }
+        // google
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+        if (requestCode == 12501) { return; }   // google account 선택 안 했을 때
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    // google login method
+    private void googleSignIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+    // google login method
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount acct = completedTask.getResult(ApiException.class);
+
+            if (acct != null) {
+                String personName = acct.getDisplayName();
+                String personGivenName = acct.getGivenName();
+                String personFamilyName = acct.getFamilyName();
+                String personEmail = acct.getEmail();
+                String personId = acct.getId();
+                Uri personPhoto = acct.getPhotoUrl();
+
+                Log.d(TAG, "handleSignInResult:personName "+personName);
+                Log.d(TAG, "handleSignInResult:personGivenName "+personGivenName);
+                Log.d(TAG, "handleSignInResult:personEmail "+personEmail);
+                Log.d(TAG, "handleSignInResult:personId "+personId);
+                Log.d(TAG, "handleSignInResult:personFamilyName "+personFamilyName);
+                Log.d(TAG, "handleSignInResult:personPhoto "+personPhoto);
+
+                onBackPressed();
+            }
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.e(TAG, "signInResult:failed code=" + e.getStatusCode());
+
+        }
+    }
+    //kakao session callback
+    public class SessionCallback implements ISessionCallback {
+
+        // 로그인에 성공한 상태
+        @Override
+        public void onSessionOpened() {
+            requestMe();
+        }
+
+        // 로그인에 실패한 상태
+        @Override
+        public void onSessionOpenFailed(KakaoException exception) {
+            Log.e("SessionCallback :: ", "onSessionOpenFailed : " + exception.getMessage());
+        }
+
+        // 사용자 정보 요청
+        public void requestMe() {
+            UserManagement.getInstance()
+                    .me(new MeV2ResponseCallback() {
+                        @Override
+                        public void onSessionClosed(ErrorResult errorResult) {
+                            Log.e("KAKAO_API", "세션이 닫혀 있음: " + errorResult);
+                        }
+
+                        @Override
+                        public void onFailure(ErrorResult errorResult) {
+                            Log.e("KAKAO_API", "사용자 정보 요청 실패: " + errorResult);
+                        }
+
+                        @Override
+                        public void onSuccess(MeV2Response result) {
+                            onBackPressed();
+                            Log.i("KAKAO_API", "사용자 아이디: " + result.getId());
+
+                            UserAccount kakaoAccount = result.getKakaoAccount();
+                            if (kakaoAccount != null) {
+
+                                // 이메일
+                                String email = kakaoAccount.getEmail();
+
+                                if (email != null) {
+                                    Log.i("KAKAO_API", "email: " + email);
+
+                                } else if (kakaoAccount.emailNeedsAgreement() == OptionalBoolean.TRUE) {
+                                    // 동의 요청 후 이메일 획득 가능
+                                    // 단, 선택 동의로 설정되어 있다면 서비스 이용 시나리오 상에서 반드시 필요한 경우에만 요청해야 합니다.
+
+                                } else {
+                                    // 이메일 획득 불가
+                                }
+
+                                // 프로필
+                                Profile profile = kakaoAccount.getProfile();
+
+                                if (profile != null) {
+                                    Log.d("KAKAO_API", "nickname: " + profile.getNickname());
+                                    Log.d("KAKAO_API", "profile image: " + profile.getProfileImageUrl());
+                                    Log.d("KAKAO_API", "thumbnail image: " + profile.getThumbnailImageUrl());
+
+                                } else if (kakaoAccount.profileNeedsAgreement() == OptionalBoolean.TRUE) {
+                                    // 동의 요청 후 프로필 정보 획득 가능
+
+                                } else {
+                                    // 프로필 획득 불가
+                                }
+                            }
+                        }
+                    });
+        }
+    }
+    //naver login method
+    void naverSignIn() {
+        mOAuthLoginModule = OAuthLogin.getInstance();
+        mOAuthLoginModule.init(
+                mContext
+                ,getString(R.string.naver_client_id)
+                ,getString(R.string.naver_client_secret)
+                ,getString(R.string.naver_client_name)
+                //,OAUTH_CALLBACK_INTENT
+                // SDK 4.1.4 버전부터는 OAUTH_CALLBACK_INTENT변수를 사용하지 않습니다.
+        );
+
+        @SuppressLint("HandlerLeak")
+        OAuthLoginHandler mOAuthLoginHandler = new OAuthLoginHandler() {
+            @Override
+            public void run(boolean success) {
+                if (success) {
+                    String accessToken = mOAuthLoginModule.getAccessToken(mContext);
+                    String refreshToken = mOAuthLoginModule.getRefreshToken(mContext);
+                    long expiresAt = mOAuthLoginModule.getExpiresAt(mContext);
+                    String tokenType = mOAuthLoginModule.getTokenType(mContext);
+
+                    Log.i("LoginData","accessToken : "+ accessToken);
+                    Log.i("LoginData","refreshToken : "+ refreshToken);
+                    Log.i("LoginData","expiresAt : "+ expiresAt);
+                    Log.i("LoginData","tokenType : "+ tokenType);
+
+                    onBackPressed();
+
+                } else {
+                    String errorCode = mOAuthLoginModule
+                            .getLastErrorCode(mContext).getCode();
+                    String errorDesc = mOAuthLoginModule.getLastErrorDesc(mContext);
+                    Toast.makeText(mContext, "errorCode:" + errorCode
+                            + ", errorDesc:" + errorDesc, Toast.LENGTH_SHORT).show();
+                }
+            };
+        };
+        mOAuthLoginModule.startOauthLoginActivity(Login.this, mOAuthLoginHandler);
+    }
+
+
+}
--- app/src/main/java/com/example/auctionapp/Mypage.java
+++ app/src/main/java/com/example/auctionapp/Mypage.java
@@ -1,19 +1,41 @@
 package com.example.auctionapp;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.kakao.usermgmt.UserManagement;
+import com.kakao.usermgmt.callback.LogoutResponseCallback;
+import com.nhn.android.naverlogin.OAuthLogin;
+
+import org.w3c.dom.Text;
+
+import java.util.concurrent.Executor;
+
 public class Mypage extends Fragment {
 
     ViewGroup viewGroup;
+
+    TextView goLogin;
+    Button logout_button;
+
+    GoogleSignInClient mGoogleSignInClient;
+    OAuthLogin mOAuthLoginModule;
 
     @Nullable
     @Override
@@ -21,12 +43,69 @@ public class Mypage extends Fragment {
         setHasOptionsMenu(true);
         viewGroup = (ViewGroup) inflater.inflate(R.layout.activity_mypage, container, false);
 
+        // google 객체
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail() // email addresses도 요청함
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this.getActivity(), gso);
+        // naver 객체
+        mOAuthLoginModule = OAuthLogin.getInstance();
+
+        logout_button = (Button) viewGroup.findViewById(R.id.logout_button);
+        logout_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                // 일반 로그인
+//                editor.putBoolean("checked", false);
+//                editor.commit();
+
+                // kakao logout
+                kakaoSignOut();
+                //google logout
+                googleSignOut();
+                //naver logout
+                naverSignOut();
+
+            }
+        });
+        goLogin = (TextView) viewGroup.findViewById(R.id.goLogin);
+        goLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(), Login.class);
+                startActivity(intent);
+            }
+        });
 
         return viewGroup;
     }
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+    }
+
+    private void kakaoSignOut() {
+        UserManagement.getInstance()
+                .requestLogout(new LogoutResponseCallback() {
+                    @Override
+                    public void onCompleteLogout() {
+                        Toast.makeText(getContext(), "로그아웃 되었습니다.", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+    //google logout
+    private void googleSignOut() {
+        mGoogleSignInClient.signOut()
+                .addOnCompleteListener(getActivity(), new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Toast.makeText(getContext(), "로그아웃 되었습니다.", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+    private void naverSignOut() {
+        mOAuthLoginModule.logout(getContext());
+        Toast.makeText(getContext(), "로그아웃 되었습니다.", Toast.LENGTH_SHORT).show();
     }
 
 }
