00642f3818b74431a0d857c7a9e62e18891f9ec8
gom1n <alice436777@gmail.com>
2021-11-08 15:38:55 +0900

    [feat] bid page_participants recyclerView

--- app/src/main/java/com/example/auctionapp/BidPage.java
+++ app/src/main/java/com/example/auctionapp/BidPage.java
@@ -1,21 +1,32 @@
 package com.example.auctionapp;
 
 import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class BidPage extends AppCompatActivity {
 
     Dialog dialog01;
+    PTAdapter ptAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bid_page);
+
+        init();
+        getData();
 
         ImageView goBack = (ImageView) findViewById(R.id.goback);
         goBack.bringToFront();
@@ -47,7 +58,11 @@ public class BidPage extends AppCompatActivity {
         goHome.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                dialog01.dismiss(); // 다이얼로그 닫기
+                dialog01.dismiss();
+
+                Intent intent = new Intent(BidPage.this, MainActivity.class);
+                startActivity(intent);
+
             }
         });
         // 참여내역 확인 버튼
@@ -57,5 +72,29 @@ public class BidPage extends AppCompatActivity {
                 dialog01.dismiss(); // 다이얼로그 닫기
             }
         });
+    }
+
+    private void init(){
+        RecyclerView ptRecyclerView = findViewById(R.id.participants_recyclerView);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext());
+        ptRecyclerView.setLayoutManager(linearLayoutManager);
+
+        ptAdapter = new PTAdapter();
+        ptRecyclerView.setAdapter(ptAdapter);
+    }
+
+    private void getData(){
+        //일단 레이아웃만
+        BidParticipants data = new BidParticipants(R.drawable.testuserimage, "참여자1", 530000, "14:46");
+        ptAdapter.addItem(data);
+        data = new BidParticipants(R.drawable.testuserimage, "참여자2", 500000, "12:46");
+        ptAdapter.addItem(data);
+        data = new BidParticipants(R.drawable.testuserimage, "참여자3", 480000, "11:46");
+        ptAdapter.addItem(data);
+        data = new BidParticipants(R.drawable.testuserimage, "참여자4", 370000, "10:46");
+        ptAdapter.addItem(data);
+
+
     }
 }
--- app/src/main/java/com/example/auctionapp/ItemDetail.java
+++ app/src/main/java/com/example/auctionapp/ItemDetail.java
@@ -8,12 +8,17 @@ import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
+
 public class ItemDetail extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_detail);
+
+        ImageView sellerImageView = (ImageView) findViewById(R.id.sellerImage);
+        Glide.with(this).load(R.drawable.testuserimage).circleCrop().into(sellerImageView);
 
         ImageView participateButton = (ImageView) findViewById(R.id.participateButton);
         participateButton.setOnClickListener(new View.OnClickListener() {
