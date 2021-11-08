c1f8e9c360a434d7229d15b171d09dbd07affb22
gom1n <alice436777@gmail.com>
2021-11-08 12:01:17 +0900

    [fix] Fix onbackpress error

--- app/src/main/java/com/example/auctionapp/ItemDetail.java
+++ app/src/main/java/com/example/auctionapp/ItemDetail.java
@@ -1,6 +1,10 @@
 package com.example.auctionapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +14,24 @@ public class ItemDetail extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_detail);
+
+        ImageView participateButton = (ImageView) findViewById(R.id.participateButton);
+        participateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), BidPage.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageView goBack = (ImageView) findViewById(R.id.goback);
+        goBack.bringToFront();
+        goBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+
     }
 }
