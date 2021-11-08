abd8799307f2f2509b7e96d7eafa354fad3b5fc0
gom1n <alice436777@gmail.com>
2021-11-08 12:36:32 +0900

    [feat] bid page

--- app/src/main/java/com/example/auctionapp/BidPage.java
+++ app/src/main/java/com/example/auctionapp/BidPage.java
@@ -1,6 +1,8 @@
 package com.example.auctionapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +12,14 @@ public class BidPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bid_page);
+
+        ImageView goBack = (ImageView) findViewById(R.id.goback);
+        goBack.bringToFront();
+        goBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
     }
 }
