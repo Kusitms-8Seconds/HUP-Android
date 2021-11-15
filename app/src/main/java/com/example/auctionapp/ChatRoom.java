3b3ceb855b381d7f1b9bed6a0ac5edb346efce33
gom1n <alice436777@gmail.com>
2021-11-15 14:50:08 +0900

    [style] chatting room_user msgbox location

--- app/src/main/java/com/example/auctionapp/ChatRoom.java
+++ app/src/main/java/com/example/auctionapp/ChatRoom.java
@@ -7,8 +7,10 @@ import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -25,9 +27,8 @@ import java.util.ArrayList;
 
 public class ChatRoom extends AppCompatActivity {
 
-    private RecyclerView chat_recyclerView;
-    private ChatAdapter adapter;
-    private RecyclerView.LayoutManager layoutManager;
+    private ListView chat_recyclerView;
+    private ChattingAdapter adapter;
     private ArrayList<User> chatList;
 
     private FirebaseDatabase database;
@@ -41,13 +42,10 @@ public class ChatRoom extends AppCompatActivity {
 //        database = FirebaseDatabase.getInstance("https://fir-chat-7bf48-default-rtdb.asia-southeast1.firebasedatabase.app");
 //        databaseReference = database.getReference("User");
 
-        chat_recyclerView = findViewById(R.id.chattingRecyclerView); // id 연결
-        chat_recyclerView.setHasFixedSize(true);
-        layoutManager = new LinearLayoutManager(this);
-        chat_recyclerView.setLayoutManager(layoutManager);
+        chat_recyclerView = findViewById(R.id.chattingRecyclerView);
         chatList = new ArrayList<>(); // 유저 객체를 담음
 
-        adapter = new ChatAdapter(chatList, this);
+        adapter = new ChattingAdapter(ChatRoom.this, chatList);
         chat_recyclerView.setAdapter(adapter);
 
         chatList.add(new User("profile","name1","안녕하세요^^"));
@@ -55,6 +53,11 @@ public class ChatRoom extends AppCompatActivity {
         chatList.add(new User("profile","name1","네! 보냈습니다."));
         chatList.add(new User("profile","user","확인했습니다. 경매상품 안전하게 보내드릴게요~"));
         chatList.add(new User("profile","user","aaa"));
+        chatList.add(new User("profile","user","aaa"));
+        chatList.add(new User("profile","user","aaa"));
+        chatList.add(new User("profile","user","aaa"));chatList.add(new User("profile","user","aaa"));chatList.add(new User("profile","user","aaa"));
+
+
 
     }
 }
@@ -95,69 +98,59 @@ class User {
     }
 }
 
-class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ChatViewHolder> {
+class ChattingAdapter extends BaseAdapter {
+    Context mContext = null;
+    LayoutInflater mLayoutInflater = null;
+    private ArrayList<User> data;
+    private ImageView userProfile;
+    private TextView userName;
+    private TextView chatContent;
 
-    private ArrayList<User> arrayList;
-    private Context context;
-    String myName;
 
-    public ChatAdapter(ArrayList<User> arrayList, Context context) {
-        this.arrayList = arrayList;
-        this.context = context;
+    public ChattingAdapter() {}
+    public ChattingAdapter(Context context, ArrayList<User> dataArray) {
+        mContext = context;
+        data = dataArray;
+        mLayoutInflater = LayoutInflater.from(mContext);
     }
 
-    @NonNull
+
     @Override
-    public ChatViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.custom_chatuser_recyclerview, parent, false);
-        ChatViewHolder holder = new ChatViewHolder(view);
-        return holder;
+    public int getCount() {
+        return data.size();
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ChatAdapter.ChatViewHolder holder, int position) {
-        holder.tv_message.setText(arrayList.get(position).getMessage());
+    public long getItemId(int position) {
+        return position;
+    }
 
-        // 사용자에 따라 메세지 detail 조정_나중에 수정     //this.name
-        if(arrayList.get(position).getName().equals("user")) {
-            holder.tv_message.setTextAlignment(View.TEXT_ALIGNMENT_TEXT_END);
-            holder.tv_message.setGravity(Gravity.RIGHT);
-            holder.tv_name.setVisibility(View.GONE);
-            holder.iv_profile.setVisibility(View.GONE);
+    @Override
+    public User getItem(int position) {
+        return data.get(position);
+    }
 
+
+    public View getView(int position, View converView, ViewGroup parent) {
+        View view;
+        // usernam에 따라 위치조정 //this.name     //임시
+        if(data.get(position).getName().equals("user")) {
+            view = mLayoutInflater.inflate(R.layout.custom_chatting_mybox, null);
         }else {
-            holder.tv_message.setTextAlignment(View.TEXT_ALIGNMENT_TEXT_START);
-            holder.tv_message.setGravity(Gravity.LEFT);
-            holder.tv_message.setBackgroundResource(R.drawable.chatbox_others);
-            holder.tv_message.setTextColor(Color.BLACK);
-            Glide.with(holder.itemView)
-                    .load(arrayList.get(position).getProfile())
-                    .into(holder.iv_profile);
-//            holder.tv_name.setText(arrayList.get(position).getName());    //getName method
-            holder.tv_name.setVisibility(View.GONE);
+            view = mLayoutInflater.inflate(R.layout.custom_chatting_otherbox, null);
         }
-    }
 
-    @Override
-    public int getItemCount() {
-        return (arrayList != null ? arrayList.size() : 0);
-    }
 
-    public class ChatViewHolder extends RecyclerView.ViewHolder {
+        userProfile = (ImageView) view.findViewById(R.id.iv_profile);
+        Glide.with(view)
+                .load(data.get(position).getProfile())
+                .into(userProfile);
+        userName = (TextView) view.findViewById(R.id.tv_name);
+        userName.setText(data.get(position).getName());
+        chatContent = (TextView) view.findViewById(R.id.tv_message);
+        chatContent.setText(data.get(position).getMessage());
 
-        ImageView iv_profile;
-        TextView tv_name;
-        TextView tv_message;
-        LinearLayout chat_layout;
-
-        public ChatViewHolder(@NonNull View itemView) {
-            super(itemView);
-            this.iv_profile = itemView.findViewById(R.id.iv_profile);
-            this.tv_name = itemView.findViewById(R.id.tv_name);
-            this.tv_message = itemView.findViewById(R.id.tv_message);
-            this.chat_layout = itemView.findViewById(R.id.chat_layout);
-
-        }
+        return view;
     }
 
 }
