d2691f23414f6e8ea821386cadf07c1e5cf7b589
gom1n <alice436777@gmail.com>
2021-11-22 04:57:33 +0900

    [style] put notice_dummy data

--- app/src/main/java/com/example/auctionapp/domain/home/view/NoticeDetail.java
+++ app/src/main/java/com/example/auctionapp/domain/home/view/NoticeDetail.java
@@ -12,6 +12,14 @@ import com.example.auctionapp.R;
 
 public class NoticeDetail extends AppCompatActivity {
 
+    String temp = "안녕하세요, HUP!입니다.\n" +
+            "\n" +
+            "더욱 발전된 서비스 제공을 위해 어플리케이션 점검이 진행될 예정입니다. 서비스 점검 시간에는 어플 사용이 잠시 중단됩니다. 사용자 여러분들의 양해를 부탁드립니다.\n" +
+            "해당시간 내에 문의 사항이 있으실 경우 HUP! 고객센터 1600-9494로 전화 부탁드립니다.\n" +
+            "\n" +
+            "점검일시\n" +
+            "2021년 11월 26일(금) 오전 3시 - 5시\n" +
+            "업데이트 시간은 상황에 따라 단축 또는 연장될 수 있습니다. ";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,8 +31,10 @@ public class NoticeDetail extends AppCompatActivity {
 
         TextView noticeTitle = (TextView) findViewById(R.id.noticeTitle);
         TextView noticeDate = (TextView) findViewById(R.id.noticeDate);
+        TextView noticeContent = (TextView) findViewById(R.id.noticeContent);
         noticeTitle.setText(title);
         noticeDate.setText(date);
+        noticeContent.setText(temp);    //dummy data
 
         ImageView goBack = (ImageView) findViewById(R.id.goback);
         goBack.bringToFront();
