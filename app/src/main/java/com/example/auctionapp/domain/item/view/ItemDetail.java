13cf5e0a6b277cb7a832e5accfdefc50405402c0
gom1n <alice436777@gmail.com>
2022-01-06 10:39:15 +0900

    [refactor] view binding_itemDetail

--- app/src/main/java/com/example/auctionapp/domain/item/view/ItemDetail.java
+++ app/src/main/java/com/example/auctionapp/domain/item/view/ItemDetail.java
@@ -23,6 +23,8 @@ import androidx.viewpager.widget.ViewPager;
 import com.bumptech.glide.Glide;
 import com.example.auctionapp.MainActivity;
 import com.example.auctionapp.R;
+import com.example.auctionapp.databinding.ActivityItemDetailBinding;
+import com.example.auctionapp.databinding.ActivityNoticeDetailBinding;
 import com.example.auctionapp.domain.home.view.Home;
 import com.example.auctionapp.domain.item.dto.DefaultResponse;
 import com.example.auctionapp.domain.item.dto.ItemDetailsResponse;
@@ -54,6 +56,7 @@ import retrofit2.Response;
 import static android.content.ContentValues.TAG;
 
 public class ItemDetail extends AppCompatActivity {
+    private ActivityItemDetailBinding binding;
 
     private ArrayList<String> itemImageList;
     private static final int DP = 24;
@@ -63,21 +66,8 @@ public class ItemDetail extends AppCompatActivity {
     private Long scrapId;
 
     public Boolean isHeart = false;
-    private ImageView heart;
-    TextView highPrice;
-    TextView participants;
-    TextView sellerName;
-    ImageView sellerImageView;
-    TextView itemName;
-    TextView itemContent;
-    TextView category;
-    TextView itemLeftTime;
-    TextView qnacount;
-    TextView deleteButton;
-    Button participateButton;
 
     ItemDetailViewPagerAdapter itemDetailViewPagerAdapter;
-    ViewPager viewPager;
 
     ArrayList<qnaData> qnaList = new ArrayList<qnaData>();
     qnaAdapter adapter;
@@ -95,21 +85,20 @@ public class ItemDetail extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_item_detail);
+        binding = ActivityItemDetailBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
 
         Intent intent = getIntent();
 //        String getItemId = intent.getExtras().getString("itemId");
         this.itemId = intent.getLongExtra("itemId",0);
 
-        sellerImageView = (ImageView) findViewById(R.id.sellerImage);
-
-        participateButton = (Button) findViewById(R.id.participateButton);
         if(Constants.userId == null) {
-            participateButton.setText("로그인 후 이용해주세요.");
-            participateButton.setEnabled(false);
-            participateButton.setBackgroundColor(Color.GRAY);
+            binding.participateButton.setText("로그인 후 이용해주세요.");
+            binding.participateButton.setEnabled(false);
+            binding.participateButton.setBackgroundColor(Color.GRAY);
         }
-        participateButton.setOnClickListener(new View.OnClickListener() {
+        binding.participateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), BidPage.class);
@@ -118,9 +107,8 @@ public class ItemDetail extends AppCompatActivity {
             }
         });
 
-        ImageView goBack = (ImageView) findViewById(R.id.goback);
-        goBack.bringToFront();
-        goBack.setOnClickListener(new View.OnClickListener() {
+        binding.goback.bringToFront();
+        binding.goback.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 onBackPressed();
@@ -130,20 +118,18 @@ public class ItemDetail extends AppCompatActivity {
         // viewpager
         this.initializeImageData();
 
-        viewPager = findViewById(R.id.itemDetailViewPager);
         itemDetailViewPagerAdapter = new ItemDetailViewPagerAdapter(this, itemImageList);
-        viewPager.setAdapter(itemDetailViewPagerAdapter);
+        binding.itemDetailViewPager.setAdapter(itemDetailViewPagerAdapter);
 
         //나중에 수정필요
-        heart = (ImageView) findViewById(R.id.isheart);
         RetrofitTool.getAPIWithAuthorizationToken(Constants.token)
                 .isCheckedHeart(ScrapCheckedRequest.of(Constants.userId, itemId))
                 .enqueue(MainRetrofitTool.getCallback(new isCheckedHeartCallback()));
         //로긘 않 해쓷ㄹ대
         if(Constants.userId==null) {
-            heart.setVisibility(View.GONE);
+            binding.isheart.setVisibility(View.GONE);
         }
-        heart.setOnClickListener(new View.OnClickListener() {
+        binding.isheart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if(isHeart) {
@@ -158,28 +144,20 @@ public class ItemDetail extends AppCompatActivity {
             }
         });
 
-        highPrice = findViewById(R.id.highPrice);
         RetrofitTool.getAPIWithAuthorizationToken(Constants.token).getMaximumPrice(itemId)
                 .enqueue(MainRetrofitTool.getCallback(new getMaximumPriceCallback()));
 
-        participants = findViewById(R.id.participants);
         RetrofitTool.getAPIWithAuthorizationToken(Constants.token).getParticipants(itemId)
                 .enqueue(MainRetrofitTool.getCallback(new getParticipantsCallback()));
 
-        sellerName = findViewById(R.id.sellerName);
         RetrofitTool.getAPIWithAuthorizationToken(Constants.token).userDetails(UserDetailsInfoRequest.of(Constants.userId))
                 .enqueue(MainRetrofitTool.getCallback(new getUserDetailsCallback()));
 
-        itemName = findViewById(R.id.itemName);
-        itemContent = findViewById(R.id.itemContent);
-        category = findViewById(R.id.category);
-        itemLeftTime = findViewById(R.id.itemLeftTime);
         RetrofitTool.getAPIWithAuthorizationToken(Constants.token).getItem(itemId)
                 .enqueue(MainRetrofitTool.getCallback(new getItemDetailsCallback()));
 
         //item delete
-        deleteButton = (TextView) findViewById(R.id.deleteButton);
-        deleteButton.setOnClickListener(new View.OnClickListener() {
+        binding.deleteButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 RetrofitTool.getAPIWithAuthorizationToken(Constants.token).deleteItem(itemId)
@@ -188,13 +166,11 @@ public class ItemDetail extends AppCompatActivity {
         });
 
         //QNA dumidata
-        qnacount = (TextView) findViewById(R.id.qaCount);
-        ListView qnaListView = (ListView) findViewById(R.id.itemDetail_qnaList);
         adapter = new qnaAdapter(this.getApplicationContext(), qnaList);
-        qnaListView.setAdapter(adapter);
+        binding.itemDetailQnaList.setAdapter(adapter);
         initializeQnAData();
 
-        qnaListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        binding.itemDetailQnaList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 qnaData data = (qnaData) parent.getItemAtPosition(position);
@@ -210,8 +186,7 @@ public class ItemDetail extends AppCompatActivity {
         });
 
         //view all qnas
-        TextView viewall = (TextView) findViewById(R.id.viewAll);
-        viewall.setOnClickListener(new View.OnClickListener() {
+        binding.viewAll.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent t = new Intent(getApplicationContext(), QnA.class);
@@ -236,7 +211,7 @@ public class ItemDetail extends AppCompatActivity {
         qnaList.add(new qnaData("기능 어떤 것들이 있나요?", "2021.11.26", "둠***", true, true));
         qnaList.add(new qnaData("자전거 브랜드 궁금합니다.", "2021.11.26", "우왕***", true, true));
 
-        qnacount.setText("(" + String.valueOf(qnaList.size()-1) + ")");
+        binding.qaCount.setText("(" + String.valueOf(qnaList.size()-1) + ")");
         adapter.notifyDataSetChanged();
     }
 
@@ -263,7 +238,7 @@ public class ItemDetail extends AppCompatActivity {
     private class deleteScrapCallback implements MainRetrofitCallback<DefaultResponse> {
         @Override
         public void onSuccessResponse(Response<DefaultResponse> response) {
-            heart.setImageResource(R.drawable.heartx);
+            binding.isheart.setImageResource(R.drawable.heartx);
             isHeart = false;
             scrapId = null;
             Log.d(TAG, "retrofit success: ");
@@ -283,7 +258,7 @@ public class ItemDetail extends AppCompatActivity {
     private class createScrapCallback implements MainRetrofitCallback<ScrapRegisterResponse> {
         @Override
         public void onSuccessResponse(Response<ScrapRegisterResponse> response) {
-            heart.setImageResource(R.drawable.hearto);
+            binding.isheart.setImageResource(R.drawable.hearto);
             isHeart = true;
             scrapId = response.body().getScrapId();
             Log.d(TAG, "retrofit success: ");
@@ -305,11 +280,11 @@ public class ItemDetail extends AppCompatActivity {
         public void onSuccessResponse(Response<ScrapCheckedResponse> response) {
 
             if(response.body().getScrapId()!=null){
-                heart.setImageResource(R.drawable.hearto);
+                binding.isheart.setImageResource(R.drawable.hearto);
                 isHeart = true;
                 scrapId = response.body().getScrapId();
             } else{
-                heart.setImageResource(R.drawable.heartx);
+                binding.isheart.setImageResource(R.drawable.heartx);
                 isHeart = false;
                 scrapId = null;
             }
@@ -332,7 +307,7 @@ public class ItemDetail extends AppCompatActivity {
         @Override
         public void onSuccessResponse(Response<MaximumPriceResponse> response) throws IOException {
 
-            highPrice.setText(String.valueOf(response.body().getMaximumPrice()));
+            binding.highPrice.setText(String.valueOf(response.body().getMaximumPrice()));
             Log.d(TAG, "retrofit success, idToken: " + response.body().toString());
         }
         @Override
@@ -349,7 +324,7 @@ public class ItemDetail extends AppCompatActivity {
 
         @Override
         public void onSuccessResponse(Response<ParticipantsResponse> response) {
-            participants.setText(String.valueOf(response.body().getParticipantsCount()));
+            binding.participants.setText(String.valueOf(response.body().getParticipantsCount()));
             Log.d(TAG, "retrofit success, idToken: " + response.body().toString());
         }
         @Override
@@ -367,9 +342,9 @@ public class ItemDetail extends AppCompatActivity {
 
         @Override
         public void onSuccessResponse(Response<UserDetailsInfoResponse> response) {
-            sellerName.setText(response.body().getUsername());
+            binding.sellerName.setText(response.body().getUsername());
             if(!response.body().getPicture().isEmpty()){
-                Glide.with(getApplicationContext()).load(response.body().getPicture()).into(sellerImageView);
+                Glide.with(getApplicationContext()).load(response.body().getPicture()).into(binding.sellerImage);
             }
             //delete item
             Log.d(TAG, "retrofit success, idToken: " + response.body().toString());
@@ -388,31 +363,31 @@ public class ItemDetail extends AppCompatActivity {
     public class getItemDetailsCallback implements MainRetrofitCallback<ItemDetailsResponse> {
         @Override
         public void onSuccessResponse(Response<ItemDetailsResponse> response) {
-            itemName.setText(response.body().getItemName());
-            itemContent.setText(response.body().getDescription());
+            binding.itemName.setText(response.body().getItemName());
+            binding.itemContent.setText(response.body().getDescription());
             if(response.body().getFileNames().size()!=0){
                 for (int i=0; i<response.body().getFileNames().size(); i++) {
                     itemImageList.add(response.body().getFileNames().get(i));
                 }
                 itemDetailViewPagerAdapter = new ItemDetailViewPagerAdapter(getApplicationContext(), itemImageList);
-                viewPager.setAdapter(itemDetailViewPagerAdapter);
+                binding.itemDetailViewPager.setAdapter(itemDetailViewPagerAdapter);
             }
-            category.setText(response.body().getCategory().getName());
+            binding.category.setText(response.body().getCategory().getName());
             LocalDateTime startDateTime = LocalDateTime.now();
             LocalDateTime endDateTime = response.body().getAuctionClosingDate();
             String days = String.valueOf(ChronoUnit.DAYS.between(startDateTime, endDateTime));
             String hours = String.valueOf(ChronoUnit.HOURS.between(startDateTime, endDateTime));
             String minutes = String.valueOf(ChronoUnit.MINUTES.between(startDateTime, endDateTime)/60);
-            itemLeftTime.setText(days+"일 "+hours+"시간 "+minutes+"분 전");
+            binding.itemLeftTime.setText(days+"일 "+hours+"시간 "+minutes+"분 전");
 
             if(response.body().getUserId().equals(myId)) {
 
-                deleteButton.setVisibility(View.VISIBLE);
+                binding.deleteButton.setVisibility(View.VISIBLE);
                 //button inactivated
-                participateButton.setEnabled(false);
-                participateButton.setBackgroundColor(Color.GRAY);
+                binding.participateButton.setEnabled(false);
+                binding.participateButton.setBackgroundColor(Color.GRAY);
             }else {
-                deleteButton.setVisibility(View.GONE);
+                binding.deleteButton.setVisibility(View.GONE);
             }
             Log.d(TAG, "retrofit success, idToken: " + response.body().toString());
         }
