d33dbdde825da1d10a8dcdaf2275b79984141ff4
gom1n <alice436777@gmail.com>
2022-04-30 09:46:28 +0900

    [style] set MeasuredViewPager for relative view

--- app/src/main/java/com/me/hurryuphup/domain/home/adapter/AuctionNowAdapter.java
+++ app/src/main/java/com/me/hurryuphup/domain/home/adapter/AuctionNowAdapter.java
@@ -6,6 +6,7 @@ import android.graphics.Typeface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
